d38e61cc4883e9605bfd3450ef52c995d0fba2cd
xyy931101 <18850198317@163.com>
2021-01-21 20:33:11 +0800

    fix:Singleton demo

--- src/main/java/com/examplexyy/demo/thread/single/Full.java
+++ src/main/java/com/examplexyy/demo/thread/single/Full.java
@@ -1,6 +1,6 @@
 package com.examplexyy.demo.thread.single;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * @Author: Xiongyy
@@ -54,5 +54,56 @@ public class Full implements Serializable,Cloneable{
 
     protected Object readResolve() {
         return INSTANCE;
+    }
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, CloneNotSupportedException, IOException, ClassNotFoundException {
+        /**
+         * 多线程获取
+         */
+        for (int i = 0; i < 10; i++) {
+            new Thread(() ->{
+                Full instance = Full.getInstance();
+                System.out.println(instance);
+            }).start();
+
+        }
+
+        /**
+         * 反杀获取
+         */
+        try{
+            Class<Full> clazz = Full.class;
+            Full full = clazz.newInstance();
+            System.out.println(full);
+        }catch (Exception e){
+            System.err.println(e);
+        }
+
+        /**
+         * 反射获取
+         */
+        try {
+
+            Full clone = Full.getInstance().clone();
+            Full clone1 = clone.clone();
+            System.out.println(clone1);
+        }catch (Exception e){
+            System.err.println(e);
+        }
+
+        //通过序列化，反序列化获取
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(Full.getInstance());
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        Full serialize = (Full) ois.readObject();
+        if (ois != null)    {ois.close();}
+        if (bis != null) {bis.close();}
+        if (oos != null) {oos.close();}
+        if (bos != null) {bos.close();}
+        System.out.println(serialize);
+
+
     }
 }
