3010651fdf4e35e8d49e9ca1667877f60fe70519
xyy931101 <18850198317@163.com>
2021-02-16 20:15:23 +0800

    fix:剪绳子

---
+++ src/main/java/com/examplexyy/demo/redis/RedisTemplateDemo.java
@@ -0,0 +1,14 @@
+package com.examplexyy.demo.redis;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @Author: Xiongyy
+ * @Date: 2021/2/16 12:16
+ * .............................................
+ * 佛祖保佑             永无BUG
+ */
+public class RedisTemplateDemo {
+
+//    public RedisTemplate
+}
