1ce6a2030985918c472016482705835eccb2111e
xyy931101 <18850198317@163.com>
2021-03-07 23:30:55 +0800

    fix:移除元素

--- src/main/java/com/examplexyy/demo/algorithm/leetcode/LengthOfLongestSubstring.java
+++ src/main/java/com/examplexyy/demo/algorithm/leetcode/LengthOfLongestSubstring.java
@@ -12,28 +12,32 @@ import java.util.Set;
  */
 public class LengthOfLongestSubstring {
 
-    public int lengthOfLongestSubstring(String s) {
-        // 哈希集合，记录每个字符是否出现过
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring(" "));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() < 1){
+            return 0;
+        }
         Set<Character> occ = new HashSet<Character>();
-        int n = s.length();
-
-        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-        int rk = -1, ans = 0;
-
-        for (int i = 0; i < n; i++) {
+        int length = s.length();
+        //每轮循环中,右指针的节点
+        int right = -1, ret = 0;
+        for(int i = 0; i < length; i++){
             if (i != 0) {
-                // 左指针向右移动一格，移除一个字符
+                // 左指针向右移动一格，移除一个字符(主要是为了移除当前字符的前一个字符)
                 occ.remove(s.charAt(i - 1));
             }
-            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+            while(right + 1 < length && !occ.contains(s.charAt(right + 1))){
                 // 不断地移动右指针
-                occ.add(s.charAt(rk + 1));
-                ++rk;
+                occ.add(s.charAt(right + 1));
+                ++right;
             }
-            // 第 i 到 rk 个字符是一个极长的无重复字符子串
-            ans = Math.max(ans, rk - i + 1);
+            //算出当前字符往后最大的无重复的字符串长度
+            ret = Math.max(ret, right - i + 1);
         }
-        return ans;
+        return ret;
     }
 
     
