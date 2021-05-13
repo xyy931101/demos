8a16348b16c24deaab1d2ba578d8fa144bc09803
xyy931101 <18850198317@163.com>
2021-05-13 22:55:34 +0800

    fix:5.13 打卡

--- src/main/java/com/examplexyy/demo/algorithm/leetcode/array/DecodeXoredPermutation1734.java
+++ src/main/java/com/examplexyy/demo/algorithm/leetcode/array/DecodeXoredPermutation1734.java
@@ -9,17 +9,43 @@ package com.examplexyy.demo.algorithm.leetcode.array;
 public class DecodeXoredPermutation1734 {
 
     public static void main(String[] args) {
-        System.out.println(7 ^ 3 ^ 4 ^ 5);
-        System.out.println(1 ^ 2 ^ 3 ^ 4);
-        int all = 1;
-        for (int i = 2; i <= 4; i++) {
-            all ^= i;
+        int[] arr = new int[] {2,4,1,5,3, 4};
+        int allXor = 0;
+        for (int i = 1; i <= arr.length; i++) {
+            allXor ^= i;
         }
-        System.out.println(all);
+
+        int a = 0;
+        for (int i = 0; i < arr.length; i++) {
+            a ^= arr[i];
+        }
+
+        System.out.println(allXor);
+        System.out.println(a);
     }
 
 
     public int[] decode(int[] encoded) {
-        return null;
+        int length = encoded.length + 1;
+        int allXor = 0;
+        //把1到n中间的所有数字都异或一遍
+        //因为条件给定了是前n个顺序的数字,也就是1~n
+        for (int i = 1; i <= length; i++) {
+            allXor ^= i;
+        }
+
+        int even = 0;
+        //数组encoded中第偶数个元素都异或一遍
+        for (int i = 1; i < length - 1; i += 2) {
+            even ^= encoded[i];
+        }
+
+        int[] res = new int[length];
+        //求出第一个值，后面就简单了
+        res[0] = allXor ^ even;
+        for (int i = 0; i < length - 1; i++) {
+            res[i + 1] = res[i] ^ encoded[i];
+        }
+        return res;
     }
 }
