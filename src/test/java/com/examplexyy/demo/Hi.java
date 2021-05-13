36f8cb65c8c4f7c8c24725888545f19673f273db
xyy931101 <18850198317@163.com>
2021-05-14 00:11:30 +0800

    fix:LRUCache

---
+++ src/test/java/com/examplexyy/demo/Hi.java
@@ -0,0 +1,181 @@
+package com.examplexyy.demo;
+
+import com.alibaba.fastjson.JSONObject;
+import com.examplexyy.demo.algorithm.MathUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @Author: Xiongyy
+ * @Date: 2020/4/7 22:43
+ * .............................................
+ * 佛祖保佑             永无BUG
+ * 佛曰:
+ * 写字楼里写字间，写字间里程序员；
+ * 程序人员写程序，又拿程序换酒钱。
+ * 酒醒只在网上坐，酒醉还来网下眠；
+ * 酒醉酒醒日复日，网上网下年复年。
+ * 但愿老死电脑间，不愿鞠躬老板前；
+ * 奔驰宝马贵者趣，公交自行程序员。
+ * 别人笑我忒疯癫，我笑自己命太贱；
+ * 不见满街漂亮妹，哪个归得程序员？
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        int[] nums = MathUtil.generateRandomArray(100, 100);
+//        int[] nums = new int[]{87,38,88,69,4,81,45,49,65,23};
+//        int[] nums = new int[]{38,69,87,81,45,49,65,88};
+        int[] copyOf = Arrays.copyOf(nums, nums.length);
+
+        System.out.println("my..................");
+        System.out.println(JSONObject.toJSONString(nums));
+        quickSort(nums);
+        System.out.println(JSONObject.toJSONString(nums));
+        Arrays.sort(copyOf);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (copyOf[i] != nums[i]){
+                System.out.println("fuck.....");
+                break;
+            }
+        }
+
+        System.out.println("nice!!!!!");
+    }
+
+    public static int[] quickSort(int[] nums) {
+        quickSelect(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private static void quickSelect(int[] nums, int leftIndex, int rightIndex) {
+        //如果左下标比右下标小,证明已经排序完了,直接返回
+        if(leftIndex >= rightIndex){
+            return;
+        }
+
+        //找到比中轴pivot大和小的数，然后做交换
+        int left = leftIndex, right = rightIndex, pivot = nums[leftIndex];
+        while (left < right){
+            while (left < right && nums[right] >= pivot){
+                right -- ;
+            }
+
+            if(left < right){
+                nums[left] = nums[right];
+            }
+
+            while (left< right && nums[left] < pivot){
+                left++;
+            }
+
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        quickSelect(nums, leftIndex, left - 1);
+        quickSelect(nums, left+ 1, rightIndex);
+
+    }
+
+
+    public static int[] findKthLargest(int[] nums, int k){
+        int kth = getKth(nums.length + 1, nums, 0, nums.length - 1);
+        int[] copyOf = Arrays.copyOf(nums, nums.length);
+        System.out.println();
+        return nums;
+    }
+
+    public static int getKth(int k, int[] nums, int start, int end) {
+
+        int pivot = nums[end];
+
+        int left = start;
+        int right = end;
+
+        while (left < right) {
+
+            while (left < right && nums [left] < pivot) {
+                left++;
+            }
+            if (left < right){
+                nums[end] = nums[left];
+            }
+            while (left < right && nums[right] >= pivot) {
+                right--;
+            }
+            nums[right] = nums[left];
+
+        }
+        nums[left] = pivot;
+        if (k == left + 1) {
+            return pivot;
+        } else if (k < left + 1) {
+            return getKth(k, nums, start, left - 1);
+        } else {
+            return getKth(k, nums, left + 1, end);
+        }
+    }
+
+    @Test
+    public void testLockSupport() throws IOException {
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "...." + i);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (i == 5) {
+                    LockSupport.park();
+                }
+            }
+        });
+        t.start();
+
+       try {
+            TimeUnit.SECONDS.sleep(7);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "after 7 seconds");
+        LockSupport.unpark(t);
+        System.in.read();
+
+    }
+
+    @Test
+    public void testLockSupport2() throws IOException {
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("t1线程启动");
+            LockSupport.park();
+            System.out.println("t1线程结束。。。。");
+        });
+        t1.start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                if (i == 5) {
+                    LockSupport.unpark(t1);
+                }
+                System.out.println(Thread.currentThread().getName() + "...." + i);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+        while (true) {
+
+        }
+
+    }
+
+}
---
+++ src/test/java/com/examplexyy/demo/JedisTest.java
@@ -0,0 +1,20 @@
+package com.examplexyy.demo;
+
+import com.examplexyy.demo.redis.JedisDemo;
+import org.junit.Test;
+
+/**
+ * @Author: Xiongyy
+ * @Date: 2020/4/19 23:34
+ * .............................................
+ * 佛祖保佑             永无BUG
+ */
+public class JedisTest {
+
+    @Test
+    public void testStr(){
+        /*JedisDemo demo = new JedisDemo();
+        demo.strTest();
+        demo.hashTest();*/
+    }
+}
