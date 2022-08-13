73fdbbd019e1016bdcaedb3d33cbb9d3f69f35f9
xyy931101 <18850198317@163.com>
2022-08-13 16:38:50 +0800

    fix:拆分

---
+++ nacos/nacos-config/src/main/java/com/xyy/nacos/xyynacos/config/web/ConfigController.java
@@ -0,0 +1,24 @@
+package com.xyy.nacos.xyynacos.config.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * @author: xiongyayun
+ * @date: 2022/8/13 14:24
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
